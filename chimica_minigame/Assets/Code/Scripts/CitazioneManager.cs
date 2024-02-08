74821a57014e213c13fae4be8dea447136a5d37f
pasqualegravante <wobblygreen@gmail.com>
2024-02-09 00:07:26 +0100

    Update - quotes screentime and scene skipping
    *Quote's screentime* is now dynamically set based on number of words and a given Words Per Seconds parameter.
    *CitazioneManager* implements instruction to force skip Benvenuto scene by pressing any key.

--- chimica_minigame/Assets/Code/Scripts/CitazioneManager.cs
+++ chimica_minigame/Assets/Code/Scripts/CitazioneManager.cs
@@ -8,26 +8,38 @@ public class CitazioniManager : MonoBehaviour
     public Color main;
     public Color lightMain;
     public TextMeshProUGUI text;
-
-    private int currentCitazione=0;
-    private string[] CITAZIONI={"\"La chimica è la poesia degli elementi.\"\nAlfred North Whitehead",
-    "\"La chimica è una sorta di magia naturale; essa coinvolge processi che sono invisibili per l'occhio umano ma che hanno un impatto enorme sulla nostra vita.\"\nKathryn Schulz"};
+    public float WPS=3; //Words Per Second(reading velocity)
+    private string[] QUOTES={
+        "\"La chimica è la poesia degli elementi.\"\nAlfred North Whitehead",
+        "\"La chimica è una sorta di magia naturale; essa coinvolge processi che sono invisibili per l'occhio umano ma che hanno un impatto enorme sulla nostra vita.\"\nKathryn Schulz",
+        "\"Nulla si crea, nulla si distrugge, tutto si trasforma.\"\nAntoine-Laurent Lavoisier"
+    };
     
-    public int changeTime=5;
+    private float[] QUOTES_READING_TIME;
+    private int currentQuote=0;
     private float currentTime=0;
+
     // Start is called before the first frame update
     void Start()
     {
+        InstatiateQuoteReadingTimes();
         changeText();
+
+        //(int)getTotalReadingTime()*2+3 --> min time for reading all quotes twice + a little offset 
+        GameObject.FindGameObjectWithTag("Manager").GetComponent<ChangeScene_Script>().SetSceneLivingSeconds((int)getTotalReadingTime()*2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.anyKeyDown){
+            GameObject.FindGameObjectWithTag("Manager").GetComponent<ChangeScene_Script>().changeScene();
+        }
+
         currentTime+=Time.deltaTime;
-        if(currentTime>=changeTime){
+        if(currentTime>=QUOTES_READING_TIME[currentQuote]){
             currentTime=0;
-            currentCitazione++;//errore di overflow, sarà fantastico se succede
+            currentQuote=(currentQuote+1)%QUOTES.Length;
             changeText();
         }
         FlashingText();
@@ -38,6 +50,22 @@ public class CitazioniManager : MonoBehaviour
     }
 
     private void changeText(){
-        text.text=CITAZIONI[currentCitazione%CITAZIONI.Length];
+        text.text=QUOTES[currentQuote];
+    }
+
+    private void InstatiateQuoteReadingTimes(){
+        QUOTES_READING_TIME=new float[QUOTES.Length];
+        for(int i=0; i<QUOTES.Length; i++){
+            QUOTES_READING_TIME[i]=QUOTES[i].Split(" ").Length/WPS;
+            Debug.Log(QUOTES_READING_TIME[i]);
+        }
+    }
+
+    private float getTotalReadingTime(){
+        float total=0;
+        foreach(float t in QUOTES_READING_TIME)
+            total+=t;
+        
+        return total;
     }
 }
